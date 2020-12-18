e538efdc8a306a791b56f71b4d8dbdae5bcf8c5a
zhangjinxuan <zhangjinxuan@geyecloud.com>
2020-12-18 16:36:32 +0800

    后端更新网关地址到8090

--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,7 @@ export default new Vuex.Store({
   state: {
     userId: -1,
     token: "",
-    ip: "http://localhost",
+    ip: "http://localhost:8090",
     minioAddress: "http://localhost:9000"
   },
   mutations: {},
